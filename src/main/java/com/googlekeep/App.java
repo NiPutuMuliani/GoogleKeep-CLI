ae669efb222dbce948bc852fb0d76acb3de5db8a
putu muliani <2415323057@pnb.ac.id>
2026-07-09 16:10:56 +0800

    Add SQLite dependency

--- src/main/java/com/googlekeep/App.java
+++ src/main/java/com/googlekeep/App.java
@@ -5,6 +5,9 @@ package com.googlekeep;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("GOOGLE KEEP CLI!");
+        System.out.println("1. Add Note");
+        System.out.println("2. View Notes");
+        System.out.println("3. Exit");
     }
 }
